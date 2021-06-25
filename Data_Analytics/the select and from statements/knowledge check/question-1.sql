2b6b9d5176446c31abaca48650f2afeb8b064c3b
mfcostner <mfcostner@gmail.com>
2021-06-25 12:00:56 -0400

    committing select & from statement knowledge check

---
+++ Data_Analytics/the select and from statements/knowledge check/question-1.sql
@@ -0,0 +1,15 @@
+-- Query 1
+select
+    facility_name,
+    customer_name,
+    item_description
+from
+    items
+
+-- Query 2
+SELECT
+    facility_name,
+    customer_name,
+    item_description
+FROM
+    items
