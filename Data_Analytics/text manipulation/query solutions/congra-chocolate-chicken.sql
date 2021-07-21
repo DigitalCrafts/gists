0ee5bd03055d5c642e3c2c6dd795950b673b7568
mfcostner <mfcostner@gmail.com>
2021-07-21 16:45:35 -0400

    conagra chocolate chicken query solution

---
+++ Data_Analytics/text manipulation/query solutions/congra-chocolate-chicken.sql
@@ -0,0 +1,20 @@
+SELECT
+    customer_name,
+    item_code,
+    item_description,
+    CASE 
+		WHEN LOWER(SUBSTRING(storage_type,1,1)) = 'f' THEN 'Freezer'
+        WHEN LOWER(SUBSTRING(storage_type,1,1)) = 'c' THEN 'Cooler'
+        ELSE 'Other'
+	END AS "storage_category",
+    tie,
+    high,
+    billing_stack_qty
+FROM
+    items
+WHERE
+    customer_id = '29244'
+    AND LOWER(SUBSTRING(storage_type,1,1)) IN ('c','f')
+    AND (LOWER(SUBSTRING(item_description, 20)) LIKE '%chicken%' OR LOWER(SUBSTRING(item_description, 1, 20)) LIKE '%chocolate%')
+LIMIT
+    100;
