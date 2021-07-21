4937b74522c5d7ebfc19fc5c9cd45b6fd7a01572
mfcostner <mfcostner@gmail.com>
2021-07-21 16:49:14 -0400

    reformat conagra chocolate cookies

--- Data_Analytics/text manipulation/query solutions/congra-chocolate-chicken.sql
+++ Data_Analytics/text manipulation/query solutions/congra-chocolate-chicken.sql
@@ -6,7 +6,7 @@ SELECT
         WHEN LOWER(SUBSTRING(storage_type,1,1)) = 'f' THEN 'Freezer'
         WHEN LOWER(SUBSTRING(storage_type,1,1)) = 'c' THEN 'Cooler'
         ELSE 'Other'
-	END AS "storage_category",
+    END AS "storage_category",
     tie,
     high,
     billing_stack_qty
