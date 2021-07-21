93ace6ab662199d4f16ec63ad1ea07b64500fc1c
mfcostner <mfcostner@gmail.com>
2021-07-21 10:55:01 -0400

    case when syntax update

--- Data_Analytics/case when statement/case-when-syntax.sql
+++ Data_Analytics/case when statement/case-when-syntax.sql
@@ -1,11 +1,11 @@
 SELECT
     column_1,
     CASE 
-        WHEN condition_1 THEN result_1
-        WHEN condition_2 THEN result_2
-        WHEN condition_3 THEN result_3
+        WHEN first_logical_expression_on_column_1 THEN result_1
+        WHEN second_logical_expression_on_column_1 THEN result_2
+        WHEN third_logical_expression_on_column_1 THEN result_3
         ...
         ELSE result_final
-    END AS "CASE WHEN column"
+    END AS "CASE WHEN column" -- it is best practice to give case/when columns an alias!
 FROM
     ...
