5986c1e2c0f2a433f46e04183f0d246715b4fac0
mfcostner <mfcostner@gmail.com>
2021-07-22 16:18:16 -0400

    distinct freezer items received

---
+++ Data_Analytics/count distinct/query exercises/distinct-freezer-items-received-in-2020.sql
@@ -0,0 +1,11 @@
+SELECT
+    im.facility_name,
+    COUNT(im.item_code) AS "No. distincts items received"
+FROM items AS im
+INNER JOIN inventory_activity AS ia ON im.item_code = ia.item_code
+    AND im.facility_id = ia.facility_id
+    AND im.customer_id = ia.customer_id
+WHERE
+    ia.activity_type = 'RECEIPT'
+    AND LOWER(im.storage_type) LIKE 'f%'
+    AND ia.filter_date BETWEEN '11-01-2020' AND '11-30-2020'
