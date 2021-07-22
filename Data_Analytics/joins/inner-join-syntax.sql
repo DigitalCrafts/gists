251ca33b855948f437cf4d3b5d625d8c559aa14a
mfcostner <mfcostner@gmail.com>
2021-07-21 21:12:37 -0400

    inner join syntax

--- Data_Analytics/joins/inner-join-syntax.sql
+++ Data_Analytics/joins/inner-join-syntax.sql
@@ -1,9 +1,9 @@
--- Using our example database
 SELECT
-    im.item_code,
-    im.item_description,
-    ia.activity,
-    ia.units
-FROM
-    items AS im
-    INNER JOIN inventory_activity AS ia ON im.item_code = ia.item_code
+    table_1.column_1,
+    table_1.column_2,
+    table_2.column_1,
+    table_2.column_2,
+    ...
+FROM 
+    table_1
+    INNER JOIN table_2 ON table_1.primary_key = table_2.foreign_key
