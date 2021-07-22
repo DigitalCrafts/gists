9db70cb22b42123ad527c93e6fccb733ea7ed4f6
mfcostner <mfcostner@gmail.com>
2021-07-21 21:09:49 -0400

    join example queries

---
+++ Data_Analytics/joins/full-join-example.sql
@@ -0,0 +1,9 @@
+-- Using our example database
+SELECT
+    im.item_code,
+    im.item_description,
+    ia.activity,
+    ia.units
+FROM
+    items AS im
+    FULL OUTER JOIN inventory_activity AS ia ON im.item_code = ia.item_code
