901c1725bc18ccbc5b7c3b91889c5cb52daaf03d
mfcostner <mfcostner@gmail.com>
2021-07-23 12:50:03 -0400

    added commas

--- Data_Analytics/design master file/re-use-column-alias.sql
+++ Data_Analytics/design master file/re-use-column-alias.sql
@@ -1,12 +1,12 @@
 -- full formula
 SELECT
-    im.tie * im.high AS hps
-    im.tie * im.high * im.billing_stack_qty AS fps
+    im.tie * im.high AS hps,
+    im.tie * im.high * im.billing_stack_qty AS fps,
     ...
 
 
 -- re-using column alias
 SELECT
-    im.tie * im.high AS hps
-    hps * im.billing_stack_qty AS fps
+    im.tie * im.high AS hps,
+    hps * im.billing_stack_qty AS fps,
     ...
