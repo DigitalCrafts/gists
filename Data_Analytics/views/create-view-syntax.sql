c09644ab12cbef7bec8a47bfa029dd1009993dc6
mfcostner <mfcostner@gmail.com>
2021-07-22 20:54:54 -0400

    clarified database name in view

--- Data_Analytics/views/create-view-syntax.sql
+++ Data_Analytics/views/create-view-syntax.sql
@@ -1,4 +1,4 @@
-CREATE VIEW database.view_name AS
+CREATE VIEW database_name.view_name AS -- database_name is the name of the database where the view is created
 SELECT
     column1, 
     column2, 
