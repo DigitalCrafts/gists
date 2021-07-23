dd7221f3678a8a33928e04574ce19f5ceb40e780
mfcostner <mfcostner@gmail.com>
2021-07-23 13:18:03 -0400

    dmf progress queries

---
+++ Data_Analytics/design master file/dmf-so-far-full-pallets.sql
@@ -0,0 +1,27 @@
+SELECT
+	im.item_code,
+    im.customer_id,
+    im.facility_id,
+    ia.filter_date,
+
+    -- input variables
+    ABS(ia.units) AS "N",
+    ABS(ia.gross_weight) AS "gw",
+    im.tie AS "t",
+    im.high AS "h",
+    im.billing_stack_qty AS "bsq",
+    t * h AS "hps",
+    hps * bsq AS "fps",
+
+    -- full pallet picks
+    FLOOR(N / fps) AS "fpp",
+    fpp * fps AS "fpq",
+    CASE WHEN fpp >= 1 THEN 1 ELSE 0 END AS "fpl",
+        
+FROM items AS im
+INNER JOIN inventory_activity AS ia ON
+	im.item_code = ia.item_code
+    AND im.facility_id = ia.facility_id
+    AND im.customer_id = ia.customer_id
+where
+	ia.activity_type = 'ORDER'
---
+++ Data_Analytics/design master file/dmf-so-far-layer-picks.sql
@@ -0,0 +1,37 @@
+SELECT
+	im.item_code,
+    im.customer_id,
+    im.facility_id,
+    ia.filter_date,
+
+    -- input variables
+    ABS(ia.units) AS "N",
+    ABS(ia.gross_weight) AS "gw",
+    im.tie AS "t",
+    im.high AS "h",
+    im.billing_stack_qty AS "bsq",
+    t * h AS "hps",
+    hps * bsq AS "fps",
+
+    -- full pallet picks
+    FLOOR(N / fps) AS "fpp",
+    fpp * fps AS "fpq",
+    CASE WHEN fpp >= 1 THEN 1 ELSE 0 END AS "fpl",
+
+    -- half pallet picks
+    FLOOR((N - fpq) / hps) AS "hpp", 
+    hpp * hps AS "hpq",
+    CASE WHEN hpp >= 1 THEN 1 ELSE 0 END AS "hpl",
+
+    -- layer picks
+    FLOOR((N - fpq - hpq) / t) AS "lp",
+    lp * t AS "lpq",
+    CASE WHEN lp >= 1 THEN 1 ELSE 0 END AS "lpl"
+        
+FROM items AS im
+INNER JOIN inventory_activity AS ia ON
+	im.item_code = ia.item_code
+    AND im.facility_id = ia.facility_id
+    AND im.customer_id = ia.customer_id
+where
+	ia.activity_type = 'ORDER'
