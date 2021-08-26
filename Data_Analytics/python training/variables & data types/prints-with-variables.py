825ef271e07ad4f7bf1c68a5f8d5b2a36e09eea9
mfcostner <mfcostner@gmail.com>
2021-08-26 18:07:04 -0400

    initial commit

---
+++ Data_Analytics/python training/variables & data types/prints-with-variables.py
@@ -0,0 +1,8 @@
+message = "This text is stored in a variable!"
+print(message)
+
+another_message = "How cool is that?"
+print(another_message)
+
+last_message = "You know, that's pretty cool"
+print(last_message)
