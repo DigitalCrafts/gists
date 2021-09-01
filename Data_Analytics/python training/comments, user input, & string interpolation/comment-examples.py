3d96d6f89768c0b1a888f052e98591da34bddb18
mfcostner <mfcostner@gmail.com>
2021-09-01 17:33:03 -0400

    comments

---
+++ Data_Analytics/python training/comments, user input, & string interpolation/comment-examples.py
@@ -0,0 +1,22 @@
+# the statement below prints the text "Hello World!" to the terminal <-- this is a great example of a single comment!
+print("Hello World!")
+
+# this is a variable which will be printed
+# to the terminal
+# ...writing all these single line comments is tedious...
+print_var = "Writing comments is great! But I wish there was a better way to write them on multiple lines"
+print(print_var)
+
+# multi-line comment using triple quotes
+"""
+Writing multi-line comments is easy!
+Just use triple quotes like I'm
+doing right now!
+"""
+
+# mult-line comment saved as a variable
+print_var_2 = """
+Well, after reading that comment now I know how!
+That's also great!
+"""
+print(print_var_2)
