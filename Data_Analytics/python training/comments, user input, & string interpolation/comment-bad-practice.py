6c9a64c6250b227938cf6c90de4f6aef0d0e19bd
mfcostner <mfcostner@gmail.com>
2021-09-02 09:58:24 -0400

    initial commit

---
+++ Data_Analytics/python training/comments, user input, & string interpolation/comment-bad-practice.py
@@ -0,0 +1,14 @@
+# creating my first variable
+first_var = "schrute" # setting the variable to the string "schrute"
+
+# creating my second variable
+second_var = "farm" # setting the variable to the string "farm"
+
+# creating my third variable
+third_var = "beets" # setting the variable to the string "beets"
+
+# combing all variables into one 
+big_var = first_var + second_var + third_var
+
+# printing big_var to the console log
+print(big_var)
---
+++ Data_Analytics/python training/comments, user input, & string interpolation/comment-best-practices.py
@@ -0,0 +1,19 @@
+def create_design_master_file(date_range, facility):
+    '''
+    Purpose: main query to create the design master file report
+    Inputs: 
+        --> these will be query parameters added to the SQL query
+        date_range: a date range over which to select data
+        facility: a specific facility 
+    Output: the completed design master file given the specific inputs
+    '''
+
+    # PROCESS
+    # 1) Get user input for date_range and facility
+    # 2) Update SQL query with user input
+    # 3) Create database connection to Redshift
+    # 4) Query database
+    # 5) Perform report calculations
+    # 6) Return final report 
+
+    pass
