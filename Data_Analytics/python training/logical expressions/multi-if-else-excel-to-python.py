f87f41510134f625c03a4f4f7172b006cbf73bb2
mfcostner <mfcostner@gmail.com>
2021-09-07 16:36:37 -0400

    initial commit

---
+++ Data_Analytics/python training/logical expressions/multi-if-else-excel-to-python.py
@@ -0,0 +1,11 @@
+storage_type = "Freezer" # column E in Excel spreadsheet
+tie = 8 # column F in Excel spreadsheet
+high = 7 # column G in Excel spreadsheet
+
+# column J in Excel spreadsheet
+if storage_type == "Freezer" and tie*high <= 56:
+    load_status = "OK to load"
+elif storage_type == "Freezer" and tie*high > 56:
+    load_status = "Not enough space to load!"
+else:
+    load_status = "Not the right storage type!"
