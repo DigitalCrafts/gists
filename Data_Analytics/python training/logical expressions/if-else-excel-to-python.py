707d5e0adc60b877066858fed88b135ae788e812
mfcostner <mfcostner@gmail.com>
2021-09-07 10:19:51 -0400

    initial commit

---
+++ Data_Analytics/python training/logical expressions/if-else-excel-to-python.py
@@ -0,0 +1,10 @@
+storage_type = "Cooler"
+
+if storage_type == "Freezer":
+    storage_temperature = "Coldest"
+
+elif storage_type == "Cooler":
+    storage_temperature = "Cold"
+
+else:
+    storage_temperature = "Not cold"
