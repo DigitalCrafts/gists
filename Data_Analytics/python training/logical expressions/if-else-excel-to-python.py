76c7a8976e438eef7cb8251158b6d79ae0710f27
mfcostner <mfcostner@gmail.com>
2021-09-07 10:29:40 -0400

    added comments

--- Data_Analytics/python training/logical expressions/if-else-excel-to-python.py
+++ Data_Analytics/python training/logical expressions/if-else-excel-to-python.py
@@ -1,10 +1,9 @@
-storage_type = "Cooler"
+storage_type = "Cooler" # column E in Excel spreadsheet
 
+# column J in Excel spreadsheet
 if storage_type == "Freezer":
     storage_temperature = "Coldest"
-
 elif storage_type == "Cooler":
     storage_temperature = "Cold"
-
 else:
     storage_temperature = "Not cold"
