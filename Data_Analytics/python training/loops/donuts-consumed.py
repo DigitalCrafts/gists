ef1b7be26d532c505f84b7d4a932cc2c6657066b
mfcostner <mfcostner@gmail.com>
2021-09-09 12:38:30 -0400

    initial commit

---
+++ Data_Analytics/python training/loops/donuts-consumed.py
@@ -0,0 +1,8 @@
+donuts_consumed = 0
+print("You have eaten %d donuts." % donuts_consumed)
+donuts_consumed += 1
+print("You have eaten %d donuts." % donuts_consumed)
+donuts_consumed += 1
+print("You have eaten %d donuts." % donuts_consumed)
+donuts_consumed += 1
+print("You have eaten %d donuts." % donuts_consumed)
