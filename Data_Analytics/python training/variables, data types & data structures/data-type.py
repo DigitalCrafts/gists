90a4bf797a283400cc1e2a18f4c15de9b4c9e1d5
mfcostner <mfcostner@gmail.com>
2021-09-10 10:41:43 -0400

    initial commit

---
+++ Data_Analytics/python training/variables, data types & data structures/data-type.py
@@ -0,0 +1,9 @@
+# declare variables
+tie = 5
+unit_weight = 4.5
+item_description = "LEMON LIME BAR"
+
+# print out variable values and corresponding data types
+print("tie", tie, type(tie))
+print("unit_weight", unit_weight, type(unit_weight))
+print("item_description", item_description, type(item_description))
