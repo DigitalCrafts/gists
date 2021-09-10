1e61862d687c507d0f6470ce0032c5cd6b6ee943
mfcostner <mfcostner@gmail.com>
2021-09-10 13:37:37 -0400

    initial commit

---
+++ Data_Analytics/python training/variables, data types & data structures/tuple-data-type.py
@@ -0,0 +1,5 @@
+customer_tuple = ("CONAGRA BRANDS, LLC", "TYSON FOODS", "PROTEIN ALLIANCE", "OK FOODS INC")
+
+print("data type:", type(customer_tuple))
+print("first customer:", customer_tuple[0])
+print("5th customer:", customer_tuple[4])
