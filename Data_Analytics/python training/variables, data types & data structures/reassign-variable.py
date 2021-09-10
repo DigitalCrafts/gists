6131fef80800689843442bd2f7c76f04d37cc2b5
mfcostner <mfcostner@gmail.com>
2021-09-10 11:17:26 -0400

    added comments

--- Data_Analytics/python training/variables, data types & data structures/reassign-variable.py
+++ Data_Analytics/python training/variables, data types & data structures/reassign-variable.py
@@ -1,8 +1,11 @@
+# no items loaded
 weight_loaded = 0
 print(weight_loaded)
 
+# first item loaded...
 weight_loaded = 500
 print(weight_loaded)
 
+# next item loaded...
 weight_loaded = 10000
 print(weight_loaded)
