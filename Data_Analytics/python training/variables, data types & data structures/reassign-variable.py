788c46771fd6a38c0d1c798f164cdcc657bf5406
mfcostner <mfcostner@gmail.com>
2021-09-10 11:21:36 -0400

    added more descriptive print

--- Data_Analytics/python training/variables, data types & data structures/reassign-variable.py
+++ Data_Analytics/python training/variables, data types & data structures/reassign-variable.py
@@ -1,11 +1,11 @@
 # no items loaded
 weight_loaded = 0
-print(weight_loaded)
+print("Weight loaded so far:", weight_loaded)
 
 # first item loaded...
 weight_loaded = 500
-print(weight_loaded)
+print("Weight loaded so far:", weight_loaded)
 
 # next item loaded...
 weight_loaded = 10000
-print(weight_loaded)
+print("Weight loaded so far:", weight_loaded)
