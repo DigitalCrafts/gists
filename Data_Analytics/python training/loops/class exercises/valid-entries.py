a3ca4d101d6265c3e169dc06279ba49e6977cb92
mfcostner <mfcostner@gmail.com>
2021-09-13 20:27:44 -0400

    added comments

--- Data_Analytics/python training/loops/class exercises/valid-entries.py
+++ Data_Analytics/python training/loops/class exercises/valid-entries.py
@@ -1,15 +1,20 @@
+# define list of valid facility IDs
 facility_ids = ["78401", "MO-SKSTN"]
 
-facility_input = input("Enter a facility: ")
+# get user input of facility ID
+facility_input = input("Enter a facility ID: ")
+
+# set up criteria for while loop
 num_entries = 0
 max_entries = 4
 
+# prompt user for a facility until either:
+# 1) user enters a valid facility
+# 2) user exceeds max number of entries
 while (facility_input not in facility_ids) and (num_entries < max_entries):
     
-    if facility_input not in facility_ids:
-
-        facility_input = input("Facility invalid. Please try again: ")
-        num_entries = num_entries + 1
+    facility_input = input("Facility ID invalid. Please try again: ")
+    num_entries = num_entries + 1
     
     if num_entries == max_entries:
         print("Too many entries - try again some other time")
