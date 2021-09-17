43764b368e550129166731d0c75df50d83d71094
mfcostner <mfcostner@gmail.com>
2021-09-17 14:48:27 -0400

    initial commit

---
+++ Data_Analytics/python training/intro to Pandas/create-df-from-item-list.py
@@ -0,0 +1,32 @@
+import pandas as pd
+
+# create items (list of dictionaries)
+items_list = [
+    {
+        'item_code': '84500', 
+        'item_description': 'CHOCOLATE SHORTCAKE', 
+        'customer_name': 'FROSTY TREATS', 
+        'gross_weight': 15.95,
+        'units':10
+    }, 
+    {
+        'item_code':'84400', 
+        'item_description':'STRAWBERRY SHORTCAKE', 
+        'customer_name':'FROSTY TREATS',
+        'gross_weight': 16.3,
+        'units':15
+    },
+    {
+        'item_code':8520,
+        'item_description':'COOKIES & CREAM BAR', 
+        'customer_name':'FROSTY TREATS',
+        'gross_weight': 14.1,
+        'units':25    
+    }
+]
+
+# convert items into a df
+items_df = pd.DataFrame(items_list)
+
+# print our items now stored as a df
+print(items_df)
