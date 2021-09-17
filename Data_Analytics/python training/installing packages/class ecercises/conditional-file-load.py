c555da135e7d5a1cbc9e366ff067e9cd4f6ba2f7
mfcostner <mfcostner@gmail.com>
2021-09-17 16:12:27 -0400

    added logic as function

--- Data_Analytics/python training/installing packages/class ecercises/conditional-file-load.py
+++ Data_Analytics/python training/installing packages/class ecercises/conditional-file-load.py
@@ -1,22 +1,30 @@
 import pandas as pd
 
+# create function to conditionally load a file
+def load_file(file_to_load):
+
+    # conditional logic to load file based on user input
+    if file_to_load == "items":
+
+        loaded_df = pd.read_csv(file_path + "/items_sample.csv")
+        return loaded_df.head()
+
+    elif file_to_load == "inventory_activity":
+        
+        loaded_df = pd.read_csv(file_path + "/inventory_activity_sample.csv")
+        return loaded_df.head()
+
+    else:
+        return "File does not exist!"
+
 # set location of local file folder
 file_path = "C:/...path to your file/"
 
 # prompt user to enter a file name
 file_to_load = input("Enter a file to load: ")
 
-# conditional logic to load file based on user input
-if file_to_load == "items":
-    loaded_df = pd.read_csv(file_path + "/items_sample.csv")
-    msg = loaded_df.head()
-
-elif file_to_load == "inventory_activity":
-    loaded_df = pd.read_csv(file_path + "/inventory_activity_sample.csv")
-    msg = loaded_df.head()
-
-else:
-    msg = "File does not exist!"
+# execute the load_file function using the user inputted file as input
+loaded_df = load_file(file_to_load)
 
 # print out first 5 lines of the file or DNE message
-print(msg)
+print(loaded_df)
