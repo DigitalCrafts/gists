bc8e0a1335cdc525e49b1e83a4c88464c8b0ae2a
mfcostner <mfcostner@gmail.com>
2021-09-17 13:39:29 -0400

    added comments

--- Data_Analytics/python training/installing packages/class ecercises/conditional-file-load.py
+++ Data_Analytics/python training/installing packages/class ecercises/conditional-file-load.py
@@ -1,20 +1,22 @@
 import pandas as pd
 
-# file_path = "C:/...path to your file/"
-file_path = "C:/Users/mfcos/Desktop/full stack analytics/DigitalCrafts/trainings/Americold/python training/lessons/session-2/installing packages"
+# set location of local file folder
+file_path = "C:/...path to your file/"
+
+# prompt user to enter a file name
 file_to_load = input("Enter a file to load: ")
 
+# conditional logic to load file based on user input
 if file_to_load == "items":
-
     loaded_df = pd.read_csv(file_path + "/items_sample.csv")
     msg = loaded_df.head()
 
 elif file_to_load == "inventory_activity":
-    
     loaded_df = pd.read_csv(file_path + "/inventory_activity_sample.csv")
     msg = loaded_df.head()
 
 else:
     msg = "File does not exist!"
 
+# print out first 5 lines of the file or DNE message
 print(msg)
