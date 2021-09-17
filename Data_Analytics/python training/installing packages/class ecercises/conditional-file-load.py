ec6358cc298685fbc91aa20ff05442e3c839b308
mfcostner <mfcostner@gmail.com>
2021-09-17 16:14:06 -0400

    added file_path as function input

--- Data_Analytics/python training/installing packages/class ecercises/conditional-file-load.py
+++ Data_Analytics/python training/installing packages/class ecercises/conditional-file-load.py
@@ -1,16 +1,14 @@
 import pandas as pd
 
 # create function to conditionally load a file
-def load_file(file_to_load):
+def load_file(file_path, file_to_load):
 
     # conditional logic to load file based on user input
     if file_to_load == "items":
-
         loaded_df = pd.read_csv(file_path + "/items_sample.csv")
         return loaded_df.head()
 
     elif file_to_load == "inventory_activity":
-        
         loaded_df = pd.read_csv(file_path + "/inventory_activity_sample.csv")
         return loaded_df.head()
 
