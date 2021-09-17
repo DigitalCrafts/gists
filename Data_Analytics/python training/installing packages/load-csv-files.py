2d2984c4bcf4d254238c49d8da24ec1549fa1612
mfcostner <mfcostner@gmail.com>
2021-09-16 20:33:39 -0400

    removed inventory_activity

--- Data_Analytics/python training/installing packages/load-csv-files.py
+++ Data_Analytics/python training/installing packages/load-csv-files.py
@@ -1,7 +1,5 @@
 import pandas as pd
 
 df_items = pd.read_csv("items.csv")
-df_inventory_activity = pd.read_csv("inventory_activity.csv")
 
 print(df_items.head())
-print(df_inventory_activity.head())
