3f66700f7076341945e42247b926434271fed270
mfcostner <mfcostner@gmail.com>
2021-09-19 14:44:13 -0400

    initial commit

---
+++ Data_Analytics/python training/joins in pandas/join-items-and-inventory-activity.py
@@ -0,0 +1,18 @@
+import pandas as pd
+
+# load items and inventory_activity from csv
+file_path = "C:/...the path to your file.../"
+df_items = pd.read_csv(file_path + "items_sample.csv")
+df_inventory_activity = pd.read_csv(file_path + "inventory_activity_sample.csv")
+
+# merge them into a single dataframe
+merged_df = pd.merge(
+    left = df_items, 
+    right = df_inventory_activity,
+    left_on = ["item_code","facility_id","customer_id"],
+    right_on = ["item_code","facility_id","customer_id"]
+)
+
+# print the first few rows and column headers
+print(merged_df.head())
+print(merged_df.columns)
