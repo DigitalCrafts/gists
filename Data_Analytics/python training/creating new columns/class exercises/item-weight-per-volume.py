1aac68133891349d3d4dd8a68dd7480e2a45c391
mfcostner <mfcostner@gmail.com>
2021-09-19 13:28:46 -0400

    added column subset and export

--- Data_Analytics/python training/creating new columns/class exercises/item-weight-per-volume.py
+++ Data_Analytics/python training/creating new columns/class exercises/item-weight-per-volume.py
@@ -12,5 +12,14 @@ items_df = items_df[items_df["item_description"].str.contains("CKN") | items_df[
 items_df["volume"] = np.ceil(items_df["unit_width"] * items_df["unit_height"] * items_df["unit_depth"])
 
 # create "weight_per_vol" by dividing gross_weight / volume --> round to 4 decimals
-items_df["weight_per_vol"] = items_df["gross_weight"] / items_df["volume"]
-items_df["weight_per_vol"] = items_df["weight_per_vol"].round(4)
+items_df["weight_per_volume"] = items_df["gross_weight"] / items_df["volume"]
+items_df["weight_per_volume"] = items_df["weight_per_volume"].round(4)
+
+# subset columns
+export_columns = ["item_code", "item_description", "storage_type", "gross_weight", "volume", "weight_per_volume"]
+items_df = items_df[export_columns]
+
+# export to a csv without the index column
+items_df.to_csv("weight_per_volume.csv", index=False)
+
+print(items_df.head())
