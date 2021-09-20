76839b9eea52082b649eee68d6d3fdd0e41730e9
mfcostner <mfcostner@gmail.com>
2021-09-19 21:21:16 -0400

    initial commit

---
+++ Data_Analytics/python training/variables and data types/class exercises/working-with-variables.py
@@ -0,0 +1,21 @@
+# declare variables for each item value
+description_1 = "CHOCOLATE SHORTCAKE"
+ti_1 = 10
+high_1 = 15
+bsq_1 = ti_1 * high_1
+
+description_2 = "STRAWBERRY SHORTCAKE"
+ti_2 = 7
+high_2 = 8
+bsq_2 = ti_2 * high_2
+
+# print all variable values
+print("item 1")
+print("Ti:", ti_1)
+print("High:", high_1)
+print("Billing stack qty:", bsq_1)
+print("")
+print("item 2")
+print("Ti:", ti_2)
+print("High:", high_2)
+print("Billing stack qty:", bsq_2)
