7c16f49da94108f7a77b719c218d5ed29b6cdd87
mfcostner <mfcostner@gmail.com>
2021-09-21 22:03:34 -0400

    remove file paths

--- Data_Analytics/python training/creating new columns/using-numpy-to-create-new-column.py
+++ Data_Analytics/python training/creating new columns/using-numpy-to-create-new-column.py
@@ -2,8 +2,7 @@ import pandas as pd
 import numpy as np
 
 # load sample items from .csv
-file_path = "C:/...the path to your file.../"
-items_df = pd.read_csv(file_path + "items_sample.csv")
+items_df = pd.read_csv("items_sample.csv")
 
 # add new columns using np functions floor/ceil
 items_df["gross_weight_rounded_down"] = np.floor(items_df["gross_weight"])
