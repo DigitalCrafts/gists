fb35203091d2eb894a8444950c6330a43d3e443e
mfcostner <mfcostner@gmail.com>
2021-09-21 21:55:08 -0400

    removed file path

--- Data_Analytics/python training/installing packages/class ecercises/conditional-file-load.py
+++ Data_Analytics/python training/installing packages/class ecercises/conditional-file-load.py
@@ -1,22 +1,19 @@
 import pandas as pd
 
 # create function to conditionally load a file
-def load_file(file_path, file_to_load):
+def load_file(file_to_load):
 
     # conditional logic to load file based on user input
     if file_to_load == "items":
-        loaded_df = pd.read_csv(file_path + "/items_sample.csv")
+        loaded_df = pd.read_csv("items_sample.csv")
         return loaded_df.head()
 
     elif file_to_load == "inventory_activity":
-        loaded_df = pd.read_csv(file_path + "/inventory_activity_sample.csv")
+        loaded_df = pd.read_csv("inventory_activity_sample.csv")
         return loaded_df.head()
 
     else:
         return "File does not exist!"
-
-# set location of local file folder
-file_path = "C:/...path to your file/"
 
 # prompt user to enter a file name
 file_to_load = input("Enter a file to load: ")
