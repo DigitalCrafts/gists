21eefd77288f3c5fc1cff803c1174e5c7e44392d
mfcostner <mfcostner@gmail.com>
2021-09-24 12:31:44 -0400

    changed "apply" to "get"

--- Data_Analytics/python training/creating new columns/apply-custom-function.py
+++ Data_Analytics/python training/creating new columns/apply-custom-function.py
@@ -4,7 +4,7 @@ import pandas as pd
 items_df = pd.read_csv("items_sample.csv")
 
 # define a custom function which returns an item's storage temperate based on it's storage type
-def apply_storage_temp(item):
+def get_storage_temp(item):
 
     storage_type = item["storage_type"]
 
@@ -16,7 +16,7 @@ def apply_storage_temp(item):
         return "Not cold"
 
 # apply custom function to dataframe
-items_df["storage_temperature"] = items_df.apply(apply_storage_temp, axis=1)
+items_df["storage_temperature"] = items_df.apply(get_storage_temp, axis=1)
 
 # specify a specific columns to display, then print the dataframe to the consol
 subset_cols = ["item_code","item_description","storage_type","storage_temperature"]
