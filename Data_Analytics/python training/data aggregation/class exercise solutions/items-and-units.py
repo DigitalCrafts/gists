0f9a0dc97acfc01c670a3bcd03982802f5f27f38
mfcostner <mfcostner@gmail.com>
2021-09-26 16:36:53 -0400

    initial commit

---
+++ Data_Analytics/python training/data aggregation/class exercise solutions/items-and-units.py
@@ -0,0 +1,15 @@
+import pandas as pd
+
+# load inventory_activity from csv
+df_inventory_activity = pd.read_csv("inventory_activity_sample.csv")
+
+# create dictionary of how each column will be aggregated
+agg_setup = {
+    "item_code":"nunique",
+    "units":'sum'
+}
+
+# aggregate inventory activity data using agg_setup
+dfg_inventory_activity = df_inventory_activity.groupby("filter_date").agg(agg_setup)
+
+print(dfg_inventory_activity)
