d39bc4929bab2a6a59599d42ce7da312d7ffbb9d
mfcostner <mfcostner@gmail.com>
2021-09-26 15:58:43 -0400

    initial commit

---
+++ Data_Analytics/python training/data aggregation/common-aggregations.py
@@ -0,0 +1,16 @@
+import pandas as pd
+
+# load inventory_activity from csv
+df_inventory_activity = pd.read_csv("inventory_activity_sample.csv")
+
+# create dictionary of how each column will be aggregated
+agg_setup = {
+    "item_code":["count","size","nunique"],
+    "units":['sum', 'mean', 'median', 'min', 'max', 'std', 'var']
+}
+
+# aggregate inventory activity data using agg_setup
+dfg_inventory_activity = df_inventory_activity.groupby("item_code").agg(agg_setup)
+
+# view aggregated data in console
+print(dfg_inventory_activity)
