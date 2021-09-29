d5ccb92b6faaed74a4180507725419c1b4f5aa59
mfcostner <mfcostner@gmail.com>
2021-09-28 22:26:01 -0400

    initial commit

---
+++ Data_Analytics/python training/design master file/querying redshift/run-query-with-params.py
@@ -0,0 +1,48 @@
+import pandas as pd
+
+# import locally stored functions and scripts
+import sql_queries
+from connect_to_db import connect_to_db
+
+# set query parameters
+facility_id = ["78401"]
+customer_id = ["91525"]
+start_date = '2021-01-01'
+end_date = '2021-12-31'
+
+# create parameter dictionary
+query_filters = {
+    "customer_id_list":tuple(customer_id),
+    "facility_id_list":tuple(facility_id),
+    "start_date":start_date,
+    "end_date":end_date
+}
+
+# specify a query to run
+query = sql_queries.ITEM_ACTIVITY
+
+# execute query against the database
+data = connect_to_db(query, query_filters)
+
+# convert data to a df
+df = pd.DataFrame(data)
+print(df.shape)
+# rename columns
+df.columns = [
+    "facility_id",
+    "item_code",
+    "item_description",
+    "item_family",
+    "storage_type",
+    "unit_depth",
+    "unit_width",
+    "unit_height",
+    "gross_weight",
+    "tie",
+    "high",
+    "billing_stack_qty",
+    "units"
+]
+
+# export dataframe to a csv
+df.to_csv("queried_data.csv", index=False)
